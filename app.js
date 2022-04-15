e0cacf00bcc512c6b657c287dfbd6a78aed5ccd8
Nirmal <nirmal.work01@gmail.com>
2022-04-15 12:58:20 +0545

    Final KTMWASH project

---
+++ app.js
@@ -0,0 +1,73 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.width = ("200px");
+
+}
+
+function hideMenu() {
+    navLinks.style.width = ("0px");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// <div class="footer">
+//         <div class="container">
+//             <div class="row">
+//                 <div class="footer-3">
+//                     <a href="#"><img src="image/logo.png" alt="logoImage" class="logoImg"></a>
+//                     <p>Lorem ipsum, dolor sit amet<br> consectetur adipisicing elit.<br> Cum facere fugiat enim
+
+//                     </p>
+//                     <div class="social-links">
+//                         <a href="#"><i class="fa fa-facebook"></i></a>
+//                         <a href="#"><i class="fa fa-instagram"></i></a>
+//                         <a href="#"><i class="fa fa-twitter"></i></a>
+//                     </div>
+
+//                 </div>
+
+//                 <div class="footer-3 ">
+//                     <h2>Services</h2>
+//                     <ul>
+//                         <a href="#">
+//                             <li>Dry Cleaning</li>
+//                         </a>
+//                         <a href="#">
+//                             <li>Ironing Services</li>
+//                         </a>
+//                         <a href="#">
+//                             <li>Pickup & Delivery</li>
+//                         </a>
+//                     </ul>
+
+//                 </div>
+
+//                 <div class="footer-3">
+//                     <h2>Get in touch</h2>
+
+//                     <i class="fa fa-map-marker" aria-hidden="true"></i><span>&nbsp;&nbsp;&nbsp;&nbsp; Baneshowr-35 ,
+//                         Kathmandu</span>
+//                     <br>
+//                     <i class="fa fa-phone" aria-hidden="true"></i><span> &nbsp;&nbsp;&nbsp;+977-9841******</span>
+//                     <br>
+//                     <i class="fa fa-envelope" aria-hidden="true"></i><span>&nbsp;&nbsp; ktmwash@gmail.com</span>
+
+
+//                 </div>
+//             </div>
+//         </div>
+
+
+
+//     </div>
